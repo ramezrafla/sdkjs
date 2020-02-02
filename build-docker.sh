250997d3fc2e0e134d071995afb348da0100d018
Ramez Rafla <ramez@zegenie.com>
2020-02-02 11:07:42 -0500

    Updating build scripts

---
+++ build-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+CONTAINER=onlyoffice-document-server
+cd build
+grunt --level=WHITESPACE_ONLY
+cd ../deploy/sdkjs/
+sudo docker cp word/sdk-all.js $CONTAINER:/var/www/onlyoffice/documentserver/sdkjs/word
+sudo docker cp word/sdk-all-min.js $CONTAINER:/var/www/onlyoffice/documentserver/sdkjs/word
+sudo docker cp slide/sdk-all.js $CONTAINER:/var/www/onlyoffice/documentserver/sdkjs/slide
+sudo docker cp slide/sdk-all-min.js $CONTAINER:/var/www/onlyoffice/documentserver/sdkjs/slide
+sudo docker cp cell/sdk-all.js $CONTAINER:/var/www/onlyoffice/documentserver/sdkjs/cell
+sudo docker cp cell/sdk-all-min.js $CONTAINER:/var/www/onlyoffice/documentserver/sdkjs/cell
+sudo docker cp common $CONTAINER:/var/www/onlyoffice/documentserver/sdkjs
--- update-docker.sh
+++ update-docker.sh
@@ -2,5 +2,6 @@
 sudo docker cp /home/ramez/temp/documentserver.tgz onlyoffice-document-server:/var/www/onlyoffice
 sudo docker exec onlyoffice-document-server bash -c "rm -rf /var/www/onlyoffice/documentserver"
 sudo docker exec onlyoffice-document-server bash -c "cd /var/www/onlyoffice/ && tar xvf documentserver.tgz"
+sudo docker exec onlyoffice-document-server bash -c "rm -f /var/www/onlyoffice/documentserver.tgz"
 sudo docker exec onlyoffice-document-server supervisorctl restart all
-./rebuild-docker.sh
+./build-docker.sh
