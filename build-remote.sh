c4201732de428504a33ba6f27e1e0eb54e71cded
Ramez Rafla <ramez@zegenie.com>
2020-02-05 11:42:10 -0500

    Updated and tested build-remote.sh

--- build-remote.sh
+++ build-remote.sh
@@ -4,10 +4,14 @@ SERVER=ubuntu@office.docsgenius.com:/var/www/onlyoffice/documentserver/sdkjs
 cd build
 grunt
 cd ../deploy/sdkjs/
+echo Pushing common
+scp -i $KEY -r common $SERVER/
+echo Pushing Document
 scp -i $KEY word/sdk-all.js $SERVER/word
 scp -i $KEY word/sdk-all-min.js $SERVER/word
+echo Pushing Presentation
 scp -i $KEY slide/sdk-all.js $SERVER/slide
 scp -i $KEY slide/sdk-all-min.js $SERVER/slide
+echo Pushing Sheet
 scp -i $KEY cell/sdk-all.js $SERVER/cell
 scp -i $KEY cell/sdk-all-min.js $SERVER/cell
-scp -i $KEY -r common $SERVER/
