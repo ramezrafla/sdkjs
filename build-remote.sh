1a1c3e4709a1804fcf07518a15d0032faa25b84c
Ramez Rafla <ramez@zegenie.com>
2020-04-17 23:47:04 -0400

    Updated remote build

--- build-remote.sh
+++ build-remote.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 KEY=~/.ssh/ec2.pem
-LOCATION=/var/www/onlyoffice/documentserver
+LOCATION=/home/ubuntu/build_tools/out/linux_64/onlyoffice/documentserver
+
 if [ -z "$DEV" ]
 then
   SERVER=ubuntu@server.office2.io
@@ -13,6 +14,7 @@ echo "Server: $SERVER"
 if [ -z "$NOBUILD" ]
 then
   echo "-- Building"
+  export APP_COPYRIGHT="Copyright (C) ZeGenie Inc 2012-2020 All rights reserved"
   export PRODUCT_VERSION="15.5.0"
   export BUILD_NUMBER="111"
   export PUBLISHER_URL="https://www.zegenie.com"
@@ -28,6 +30,6 @@ then
   rm -f sdkjs.tgz > /dev/null
   tar czf sdkjs.tgz sdkjs
   scp -i $KEY sdkjs.tgz $SERVER:$LOCATION
-  ssh -i $KEY $SERVER "cd $LOCATION && sudo chown ubuntu:ubuntu -R sdkjs && tar xvf sdkjs.tgz"
+  ssh -i $KEY $SERVER "cd $LOCATION && tar xvf sdkjs.tgz"
   rm -f sdkjs.tgz > /dev/null
 fi
